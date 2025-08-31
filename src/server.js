f20cd693dc7ed3f070afd6141175c95240509730
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2025-08-31 13:34:53 -0300

    feat(server): adicionar wagger para documentação.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,6 +12,12 @@ const productRoutes = require('./modules/products/product.routes');
 const supplierRoutes = require('./modules/suppliers/supplier.routes');
 const userRoutes = require('./modules/auth/user.routes');
 
+// Docs (Swagger Ui)
+const openapiPath = path.join(__dirname, 'openapi.json');
+const openapiDoc = JSON.parse(fs.readFileSync(openapiPath, 'utf-8'));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiDoc));
+
+// API routes
 app.use('/api', productRoutes);
 app.use('/api', supplierRoutes);
 app.use('/api', userRoutes);
