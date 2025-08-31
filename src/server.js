74af6d45c2e4bc800c5d2df41488685027f45f5d
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2025-08-31 00:46:13 -0300

    feat(server): implementa a rota de produtos no servidor

--- src/server.js
+++ src/server.js
@@ -3,6 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
+const productRoutes = require('./modules/products/product.routes');
+
+app.use('/api', productRoutes);
+
 app.listen(PORT, () => {
     console.log(`Servidor executando em http://localhost:${PORT}`);
 })
